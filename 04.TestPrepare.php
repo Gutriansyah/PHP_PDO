4b5c93fc4c38666c566b3496eb69a6c419224a01
Giri Gutriansyah <gitrigutriansyah@gmail.com>
2023-03-11 20:49:12 +0700

    membuat file Note.txt

--- 04.TestPrepare.php
+++ 04.TestPrepare.php
@@ -5,26 +5,25 @@ use function Helper\getConnection;
 require_once __DIR__ . "/Helper/getConnection.php";
 
 $connection = getConnection();
-
 $username = "admin";
 $password = "admin";
-$sql = "SELECT * FROM admin username = :username AND password = :password";
+
+$sql = "SELECT * FROM admin WHERE username = :username AND password = :password";
 $result = $connection->prepare($sql);
 $result->bindParam("username", $username);
 $result->bindParam("password", $password);
 $result->execute();
 
-// $succes = false;
-// foreach ($result as $row) {
-//     $succes = true;
-//     $name = $row["username"];
-// }
+$succes = false;
+foreach ($result as $row) {
+    $succes = true;
+    $name = $row["username"];
+}
 
-// if ($succes) {
-//     echo "Login Berhasil  " . PHP_EOL;
-// } else {
-//     echo "Login Gagal" . PHP_EOL;
-// }
-
+if ($succes) {
+    echo "Login Berhasil $name" . PHP_EOL;
+} else {
+    echo "Login Gagal" . PHP_EOL;
+}
 
 $connection = null;
