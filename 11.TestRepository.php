855fae1fc37a76d3cac98ac4b8f5e956fd6c915e
Giri Gutriansyah <gitrigutriansyah@gmail.com>
2023-03-13 09:15:47 +0700

    menambahka komnetar pada kode program

--- 11.TestRepository.php
+++ 11.TestRepository.php
@@ -9,19 +9,24 @@ require_once __DIR__ . "/Helper/getConnection.php";
 require_once __DIR__ . "/Model/Comment.php";
 require_once __DIR__ . "/Repository/CommentRepository.php";
 
-
+// * memanggil fungsi koneksi ke database yang disimpan ke dalam variable $connection
 $connection = getConnection();
-$repos = new CommentRepositoryImpl($connection);
+// * memanggil object class Comment Repository yang memiliki parameter bertipe PDO 
+$repository = new CommentRepositoryImpl($connection);
 
-$comment = new Comment(email: "repos@test", comment: "hy");
-$newComment = $repos->insert($comment);
+// * membuat object dari class Model/Comment beserta parameternya
+$comment = new Comment(email: "rom@test", comment: "hyyy");
+// * memanggil function insert dari object repository yang memiliki parameter bertipe object Comment
+$newComment = $repository->insert($comment);
+// * kenapa di vardump new comment
+var_dump($newComment->getId()) . PHP_EOL;
+var_dump($newComment->getEmail()) . PHP_EOL;
+var_dump($newComment->getComment()) . PHP_EOL;
 
-var_dump($newComment->getId());
+// $comment = $repository->findById(10);
+// var_dump($comment);
 
-// $comment = $repos->findById(10);
-// var_dump($comment);  
-
-$comment = $repos->findAll();
-var_dump($comment);
+// $comment = $repository->findAll();
+// var_dump($comment);
 
 $connection = null;
--- Repository/CommentRepository.php
+++ Repository/CommentRepository.php
@@ -18,27 +18,34 @@ interface CommentRepository
     function findAll(): array;
 }
 
-
+// * class Comment repositroy memiliki property yang bertipe data PDO
 class CommentRepositoryImpl implements CommentRepository
 {
 
     private PDO $connection;
 
+    // * membuat cintruc sebagai parameter object yang memiliki tipe data PDO
     public function __construct(PDO $connection)
     {
         $this->connection = $connection;
     }
 
+    // * membuat function insert yang memiliki parameter Object Class Comment
     function insert(Comment $comment): Comment
     {
-        // $connection = getConnection();
+        // * membuat query sql yang valuenya berasal dari objetc comment
         $sql = "INSERT INTO comments(email, comment) VALUES (?,?)";
+        // * melakukan prepare
         $statment = $this->connection->prepare($sql);
+        // * melakukan esekusi perintah sql dan melakukan binding pada parameter yang diambil dari Properti class Comment
         $statment->execute([$comment->getEmail(), $comment->getComment()]);
 
+        // * meakukan pengambilan id yang terakhir di input menggunakan function lastInsertId
         $id = $this->connection->lastInsertId();
+        // * melakukan setId untuk mengisi properti dari Model/Comment dikarenkan waktu isnert kita tidak menginsertkan id 
         $comment->setId($id);
 
+        // * melakukan return comment
         return $comment;
     }
 
